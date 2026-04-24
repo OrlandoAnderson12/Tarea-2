7b0af96c3cbcacf4ea7686bd27565e62f5cac90f
OrlandoAnderson12 <opad16102007@gmail.com>
2026-04-24 09:50:28 -0400

    Create problema3_1.c.cpp

---
+++ problema3_1.c.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+    int Sal;
+    double nuevosalario;
+    printf("Ingrese el salario anual en pesos:");
+    scanf("%d", &Sal);
+    if (Sal < 18000) { nuevosalario = Sal * 1.12;
+        printf("El salario es menor a 18,000 pesos y incremento un 12 porciento.\n");
+        printf("Salario original: %d\n", Sal);
+        printf("Nuevo salario: %.2f\n", nuevosalario);
+    } else if (Sal <= 30000) {
+        nuevosalario = Sal * 1.08;
+        printf("El salario esta entre 18,000 y 30,000 pesos entonces aumento un 8 porciento.\n");
+        printf("Salario original: %d\n", Sal);
+        printf("Nuevo salario: %.2f\n", nuevosalario);
+    } else if (Sal <= 50000) {
+        nuevosalario = Sal * 1.07;
+        printf("El salario esta entre 30,000 y 50,000 pesos entonces aumento un 7 porciento.\n");
+        printf("Salario original: %d\n", Sal);
+        printf("Nuevo salario: %.2f\n", nuevosalario);
+    } else if (Sal > 50000) {
+    nuevosalario = Sal * 1.06;
+    printf("El salario es mayor a 50,000 pesos entonces aumento un 6 porciento.\n");
+    printf("Salario original: %d\n", Sal);
+    printf("Nuevo salario: %.2f\n", nuevosalario);
+    }
+    return 0;
+     }
